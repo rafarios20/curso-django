955c95723e0dc4a98d2224013586b3436e3d4a01
Rafael <rafaelsrios12@gmail.com>
2022-04-20 11:43:50 -0300

    Atualização do flake8 close#3

--- pypro/settings.py
+++ pypro/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lhi*heuz((dxt2z=d$7r8z^9og=!6iyik@qsy@ah$c!fu(yrw+'
+SECRET_KEY =\
+    'django-insecure-lhi*heuz((dxt2z=d$7r8z^9og=!6iyik@qsy@ah$c!fu(yrw+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
