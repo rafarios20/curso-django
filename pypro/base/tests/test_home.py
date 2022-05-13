6258b12d8196e709428eadb95322a60ed623e872
Rafael <rafaelsrios12@gmail.com>
2022-05-13 16:16:47 -0300

    Configurando urls nomeadas close #66

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,12 +1,19 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
 
 from pypro.django_assertions import assert_contains
 
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('home'))
+    return resp
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+def test_status_code(resp):
     assert resp.status_code == 200
 
-def test_title (client: Client):
-    resp = client.get('/')
+def test_title (resp):
     assert_contains(resp, '<title>Python Pro</title>')
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("home")}">Python Pro</a>')
