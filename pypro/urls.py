37f8673c09660612b33166af539b0f0c43b6e7fb
Rafael <rafaelsrios12@gmail.com>
2022-05-16 16:10:09 -0300

    Encapsulando urls close #72

--- pypro/urls.py
+++ pypro/urls.py
@@ -17,11 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', home, name='home'),
+    path('', include('pypro.base.urls')),
 ]
 
 if settings.DEBUG:
