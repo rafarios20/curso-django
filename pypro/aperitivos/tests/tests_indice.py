77fd9da1978177d4aafb3da94757982bf1736a32
Rafael <rafaelsrios12@gmail.com>
2022-05-26 15:48:37 -0300

    Acesso a método em template close #104

--- pypro/aperitivos/tests/tests_indice.py
+++ pypro/aperitivos/tests/tests_indice.py
@@ -8,8 +8,10 @@ from pypro.django_assertions import assert_contains
 def resp(client):
     return client.get(reverse('aperitivos:indice'))
 
+
 def test_status_code(resp):
-    assert resp.status_code==200
+    assert resp.status_code == 200
+
 
 @pytest.mark.parametrize(
     'titulo',
@@ -20,6 +22,15 @@ def test_status_code(resp):
 )
 def test_titulo_video(resp, titulo):
     assert_contains(resp, titulo)
-#
-# def test_conteudo_video(resp):
-#     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/713008559?h=e310c49b45&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"')
+
+
+@pytest.mark.parametrize(
+    'slug',
+    [
+        'motivacao',
+        'instalacao-windows'
+    ]
+)
+def test_link_video(resp, slug):
+    video_link = reverse('aperitivos:video', args=(slug,))
+    assert_contains(resp, f'href="{video_link}"')
