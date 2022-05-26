0d843ae2458ca7facb38fff2e7301f7ed38d2919
Rafael <rafaelsrios12@gmail.com>
2022-05-26 12:23:23 -0300

    Acesso a atributos do template

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+
 videos = [
-    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 713008559},
-    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'vimeo_id': 713008559}
+    Video('motivacao', 'Video Aperitivo: Motivação', 713008559),
+    Video('instalacao-windows', 'Instalação Windows', 713008559),
 ]
 
-videos_dct={dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
+
+
 def indice(request):
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
