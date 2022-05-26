5ebc9359f03b077c864d9c01d633da624ca0b681
Rafael <rafaelsrios12@gmail.com>
2022-05-26 16:16:18 -0300

    Criando composição de template close #106

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -17,3 +17,6 @@ def test_title (resp):
 
 def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
+
+def test_email_link(resp):
+    assert_contains(resp, 'href="mailto:ramalho@python.pro.br"')
